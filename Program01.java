db0f1c59444e769d9eace36db26471ff90127405
darnold <arnold.dobo@gmail.com>
2017-08-15 12:41:31 +0200

    getTriangleArea

--- Program01.java
+++ Program01.java
@@ -2,6 +2,10 @@ class Program01 {
 	public static double getTriangleRange(double a, double b, double c) {
 		return a + b + c;
 	}	
+	public static doubel getTriangleArea() {
+		double s = (a + b + c) /2;
+		return Math.sqrt(s*(s-a)*(s-b)*(s-c));
+	}	
 	public static void main(String[] args) {
 		System.out.println("Hi");
 	}
