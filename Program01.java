f71df81a4904eee2c527a91be8ee865a2849616c
darnold <arnold.dobo@gmail.com>
2017-08-15 12:36:24 +0200

    jegy

--- Program01.java
+++ Program01.java
@@ -2,6 +2,9 @@ class Program01 {
 	public static void szamitHaromszog() {
 		int 3 + 4 + 5;
 	}
+	public static void jegy() {
+		System.out.println("Nevem jön ide");
+	}	
 	public static void main(String[] args) {
 		System.out.println("Hi");
 	}
