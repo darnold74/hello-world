d0097dfa436fe8338e9ada1f178401a5fe2cf09f
darnold <arnold.dobo@gmail.com>
2017-08-15 12:36:53 +0200

    ertek

--- Program01.java
+++ Program01.java
@@ -5,6 +5,9 @@ class Program01 {
 	public static void jegy() {
 		System.out.println("Nevem jön ide");
 	}	
+	public static void ertek() {
+		System.out.println("Értéke ez.");
+	}	
 	public static void main(String[] args) {
 		System.out.println("Hi");
 	}
